d14bc0f6959c365bd6d42f61f74ac9b0a8be2a62
Jepoy <martjepoy@gmail.com>
2025-11-11 11:43:47 +0800

    Activity 2: In-memory authentication setup

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-final class DashboardController extends AbstractController
+class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        return new Response('Welcome to dashboard! (Protected Area)');
+        $user = $this->getUser();
+        $role = in_array('ROLE_ADMIN', $user->getRoles()) ? 'admin' : 'user';
+
+        return $this->render('dashboard/index.html.twig', [
+            'role' => $role,
+        ]);
     }
 }
